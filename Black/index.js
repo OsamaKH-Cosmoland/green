079b53c886a9296458bb9423e52c3baba28b005e
osamakahireldin <osamakahireldin@gmail.com>
2023-07-04 23:50:03 +0300

    add commit

--- Black/index.js
+++ Black/index.js
@@ -1,1 +1,21 @@
-console.log('hello world!')
+console.log("Hello World");
+
+const east = document.createElement("east");
+const west = document.createElement("west");
+west.setAttribute("class", "hoba hoba is the best");
+east.appendChild(west);
+
+const codeSnippet = `
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+    
+    </body>
+    </html>
+`;
+
+console.log(codeSnippet);
